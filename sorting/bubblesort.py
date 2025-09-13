560d0321bc54719afdfef9d2067d80450a69c31c
mohamednabil1406 <connectwithmhd@gmail.com>
2025-09-13 11:26:59 +0530

    first commit

---
+++ sorting/bubblesort.py
@@ -0,0 +1,13 @@
+def bubblesort(arr):
+    n=len(arr)
+    for  i in range(n):
+        swapped=False
+        for j in range(0,n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+                swapped=True
+        if not swapped:
+            break
+    return arr
+arr=[5,2,9,1]
+print(bubblesort(arr))
