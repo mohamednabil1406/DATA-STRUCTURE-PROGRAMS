54ac297270c3fa1ea0384d738eaff3f7fa70ca87
mohamednabil1406 <connectwithmhd@gmail.com>
2025-09-20 20:04:02 +0530

    new

---
+++ array/sortarr)sand1s.py
@@ -0,0 +1,20 @@
+def sortarr(arr):
+    low=0
+    mid=0
+    high=len(arr)-1
+    while mid<=high:
+        if arr[mid]==0:
+            arr[low],arr[mid]=arr[mid],arr[low]
+            low+=1
+            mid+=1
+        elif arr[mid]==1:
+            mid+=1
+        else:
+            arr[mid],arr[high]=arr[high],arr[mid]
+            high-=1
+n = 6
+arr = [0, 2, 1, 2, 0, 1]
+sortarr(arr)
+for num in arr:
+    print(num, end=" ")
+print()
